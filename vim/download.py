48349bbd96d821e2fe878510f7e6fdbef8fd1d75
Danqi Wang <beyondwdq@gmail.com>
2016-10-16 23:05:31 +0100

    replcae double quotes with single quotes

--- vim/download.py
+++ vim/download.py
@@ -7,8 +7,8 @@ import subprocess
 
 
 cmdstr_mapping = {
-        "git": (["git", "clone"], ["git", "pull"]),
-        "hg": (["hg", "clone"], ["hg", "pull"])
+        'git': (['git', 'clone'], ['git', 'pull']),
+        'hg': (['hg', 'clone'], ['hg', 'pull'])
         }
 
 
@@ -18,11 +18,11 @@ def get_command_string(cmd, update):
 
 def get_command_for_url(url):
     cmdprot_mapping = {
-            "git": "git",
-            "hg": "hg"
+            'git': 'git',
+            'hg': 'hg'
             }
 
-    cmd = "git"
+    cmd = 'git'
     protocol = url.split(':')[0]
     if protocol in cmdprot_mapping:
         cmd = cmdprot_mapping[protocol]
@@ -36,13 +36,13 @@ def get_command_for_url(url):
 
 def clone_repos(cmd, url):
     full_cmd = get_command_string(cmd, update=False) + [url]
-    print "running command: " + " ".join(full_cmd)
+    print 'running command: ' + ' '.join(full_cmd)
     subprocess.call(full_cmd)
 
 
 def update_repos(cmd, dirname):
     full_cmd = get_command_string(cmd, update=True)
-    print "running command: " + " ".join(full_cmd)
+    print 'running command: ' + ' '.join(full_cmd)
     subprocess.call(full_cmd, cwd=dirname)
 
 
@@ -61,7 +61,7 @@ def run_scripts(dirname):
     script_name = 'post_update.sh'
     script_path = os.path.join(root_dir, dirname, script_name)
     if os.path.exists(script_path):
-        print "Running script for %s" % dirname
+        print 'Running script for %s' % dirname
         subprocess.call([script_path, dirpath])
 
 
@@ -74,24 +74,24 @@ def parse_line(line):
         url = items[0]
         cmd = get_command_for_url(url)
     else:
-        raise ValueError("Invalid line: %s" % line)
+        raise ValueError('Invalid line: %s' % line)
     return cmd, url
 
 
 def show_stats(update, cloned, updated, skipped):
-    print "Number cloned:%s" % len(cloned)
+    print 'Number cloned:%s' % len(cloned)
 
     if (len(cloned)):
-        print "\t\n".join(cloned)
+        print '\t\n'.join(cloned)
 
     if update:
-        print "Number updated:%s" % len(updated)
+        print 'Number updated:%s' % len(updated)
         if (len(updated)):
-            print "\t\n".join(updated)
+            print '\t\n'.join(updated)
     else:
-        print "Number skipped:%s" % len(skipped)
+        print 'Number skipped:%s' % len(skipped)
         if (len(skipped)):
-            print "\t\n".join(skipped)
+            print '\t\n'.join(skipped)
 
 
 def load_lines(filename):
@@ -111,22 +111,22 @@ def run(update=False):
     updated = []
     skipped = []
 
-    for line in load_lines("repos_urls.txt"):
+    for line in load_lines('repos_urls.txt'):
         cmd, url = parse_line(line)
         dirname = get_dirname(url)
 
         do_run_script = False
         if bundle_exists(dirname):
             if update:
-                print "Updating %s in dir:%s" % (url, dirname)
+                print 'Updating %s in dir:%s' % (url, dirname)
                 update_repos(cmd, dirname)
                 updated.append(url)
                 do_run_script = True
             else:
-                print "Skipping %s" % url
+                print 'Skipping %s' % url
                 skipped.append(url)
         else:
-            print "Cloning %s" % url
+            print 'Cloning %s' % url
             clone_repos(cmd, url)
             cloned.append(url)
             do_run_script = True
@@ -134,7 +134,7 @@ def run(update=False):
         if do_run_script:
             run_scripts(dirname)
 
-        print ""
+        print ''
 
     show_stats(update, cloned, updated, skipped)
 
@@ -142,7 +142,7 @@ def run(update=False):
 def main():
 
     update = False
-    if len(sys.argv) == 2 and sys.argv[1] == "-u":
+    if len(sys.argv) == 2 and sys.argv[1] == '-u':
         update = True
 
     run(update=update)
