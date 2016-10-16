2a459877e08650944233d1c67e001e0169f5fdcd
Danqi Wang <beyondwdq@gmail.com>
2016-10-16 15:55:12 +0100

    move scripts from vim/bundle to vim

--- vim/install.sh
+++ vim/install.sh
@@ -4,5 +4,5 @@ srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p $HOME/.vim/bundle
 ln -sf $srcdir/.vimrc $HOME/
-ln -sf $srcdir/bundle/download.py $HOME/.vim/bundle/
-ln -sf $srcdir/bundle/repos_urls.txt $HOME/.vim/bundle/
+ln -sf $srcdir/download.py $HOME/.vim/bundle/
+ln -sf $srcdir/repos_urls.txt $HOME/.vim/bundle/
