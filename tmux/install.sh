7a580d96e812936cd5131f58207c226c114f0140
dwang <dwang@drwsg.com>
2017-12-05 20:16:23 -0600

    tmux: tpm and resurrect

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,4 +2,7 @@
 
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "Fetching tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Linking config"
 ln -sf $srcdir/.tmux.conf $HOME/
