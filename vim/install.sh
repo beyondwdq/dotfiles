f7a56f962fd611e6401ab7ea34b6b5ceaf33ef9c
Danqi <beyondwdq@gmail.com>
2022-04-23 12:51:36 +0800

    vim install: download pathogen

--- vim/install.sh
+++ vim/install.sh
@@ -2,7 +2,9 @@
 
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p $HOME/.vim/bundle
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 ln -sf $srcdir/.vimrc $HOME/
 ln -sf $srcdir/download.py $HOME/.vim/bundle/
 ln -sf $srcdir/repos_urls.txt $HOME/.vim/bundle/
